6342a05f82ac39b528bd37cb39a55123a97caa1e
Antonio Erdeljac <hedhogs@gmail.com>
2020-10-04 04:33:03 +0200

    Finish punching bag game

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+const DEFAULT_HEALTH = 100;
+const DEFAULT_DAMAGE = 10;
+
 new Vue({
   el: '#vue-app',
-  data: {},
-  methods: {},
+  data: {
+    health: DEFAULT_HEALTH,
+    damage: DEFAULT_DAMAGE,
+  },
+  methods: {
+    punch: function() {
+      this.health -= this.damage;
+    },
+    restart: function() {
+      this.health = DEFAULT_HEALTH;
+    }
+  },
+  computed: {
+    hasFinished: function() {
+      return this.health <= 0;
+    },
+    remainingHealth: function() {
+      return 100 - this.health;
+    }
+  }
 });
